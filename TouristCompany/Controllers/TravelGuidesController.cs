96292fe4e28f7a9e6cd7d6b6923576e4ebd21d0a
בלוי שאשא < @mbjcomp.local>
2024-02-25 16:11:18 +0200

    Adding controllers

---
+++ TouristCompany/Controllers/TravelGuidesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlTour;
+using BlTour.BlApi;
+using BlTour.BlModels;
+
+namespace TouristCompany.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TravelGuidesController : ControllerBase
+    {
+        IBlTravelGuide blTravelGuide;
+        public TravelGuidesController(BLManager bl)
+        {
+            this.blTravelGuide = bl.TravelGuides;
+        }
+        [HttpGet]
+        public ActionResult<List<BlTravelGuide>> GetAll()
+        {
+            return blTravelGuide.GetAll();
+        }
+        [HttpGet("{Id}")]
+        public ActionResult<BlTravelGuide> Get(int id)
+        {
+            return blTravelGuide.Get(id);
+        }
+        [HttpPost]
+        
+
+    }
+}
---
+++ TouristCompany/Controllers/TravelersGroupsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TouristCompany.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TravelersGroupsController : ControllerBase
+    {
+    }
+}
